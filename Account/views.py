09532dd6378fe59e043b61d2ea8c8d5237b9423b
Göksel Kınalı <carryvayne12211@gmail.com>
2021-12-02 11:50:21 +0300

    Post Sorunu Halledildi ve Registerdaki şifre eşlememe durumu eklendi.

--- Account/views.py
+++ Account/views.py
@@ -1,11 +1,29 @@
 from django.http.response import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib import messages
 
 # Create your views here.
 
 class Kullanıcı:
     Isım=str
     soyisim=str
+
+def register(request):
+    
+        if request.method == 'POST':
+            firstname=request.POST['first_name']
+            lastname=request.POST['last_name']
+            password1=request.POST['password1']
+            password2=request.POST['password2']
+            username=request.POST['username']
+            email=request.POST['email']
+
+            #şifreler aynı mı
+            if password1!= password2:
+                    messages.info(request,'Password Does not Match ! ')
+            return redirect('http://127.0.0.1:8000/')
+
+        return render(request,'register.html')
 
 def home(request):
 
