9b35e0678175ca87abb0825e1695d7241f49a680
Göksel Kınalı <carryvayne12211@gmail.com>
2021-12-03 19:27:34 +0300

    File modeli tekrar oluştı.

--- Account/models.py
+++ Account/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.db.models.fields import IntegerField
+from django.db.models.fields.files import FileField
 
 # Create your models here.
 class Kullanicilar(models.Model):
@@ -7,3 +9,7 @@ class Kullanicilar(models.Model):
     email=models.CharField(max_length=50)
     first_name=models.CharField(max_length=50)
     last_name=models.CharField(max_length=50)
+class File(models.Model):
+    file=FileField(upload_to='pics')
+    userid=IntegerField()
+ 
