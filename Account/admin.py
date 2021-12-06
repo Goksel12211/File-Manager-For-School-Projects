0bab37925315aa912e94d4f852d0304585ccad51
Göksel Kınalı <carryvayne12211@gmail.com>
2021-12-07 01:41:21 +0300

    Admin şifre değişti.

--- Account/admin.py
+++ Account/admin.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
-from .models import Kullanicilar,File
+from .models import Kullanicilar,File,Danisman,Juri,Anahtar_Kelimeler,Proje_Ozellikleri,Yazar
 
     # Register your models here.
 
 admin.site.register(Kullanicilar)
 admin.site.register(File)
+admin.site.register(Danisman)
+admin.site.register(Juri)
+admin.site.register(Anahtar_Kelimeler)
+admin.site.register(Proje_Ozellikleri)
+admin.site.register(Yazar)
 
 admin.site.site_title="TEK ADMIN UMUTTUR"
 
